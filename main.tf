8b793686b7990611c9c9da55b672e9cf51187087
looneym <looneymicheal@gmail.com>
2018-03-13 20:56:17 +0000

    Simplify key name and path to file

--- main.tf
+++ main.tf
@@ -1,21 +1,11 @@
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.2"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  attributes = "${var.attributes}"
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
-}
-
 locals {
-  public_key_filename  = "${var.ssh_public_key_path}/${module.label.id}${var.public_key_extension}"
-  private_key_filename = "${var.ssh_public_key_path}/${module.label.id}${var.private_key_extension}"
+  public_key_filename  = "${var.ssh_public_key_path}/${var.name}${var.public_key_extension}"
+  private_key_filename = "${var.ssh_public_key_path}/${var.name}${var.private_key_extension}"
 }
 
 resource "aws_key_pair" "imported" {
   count      = "${var.generate_ssh_key == "false" ? 1 : 0}"
-  key_name   = "${module.label.id}"
+  key_name   = "${var.name}"
   public_key = "${file("${local.public_key_filename}")}"
 }
 
@@ -27,7 +17,7 @@ resource "tls_private_key" "default" {
 resource "aws_key_pair" "generated" {
   count      = "${var.generate_ssh_key == "true" ? 1 : 0}"
   depends_on = ["tls_private_key.default"]
-  key_name   = "${module.label.id}"
+  key_name   = "${var.name}"
   public_key = "${tls_private_key.default.public_key_openssh}"
 }
 
